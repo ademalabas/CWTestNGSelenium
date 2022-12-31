2c4bad34571ae66cb4a002bd9adf335a75c1507f
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-31 12:55:26 +0300

    31.12.22

---
+++ src/test/java/clarusway/tests/Day_01/C06_Description.java
@@ -0,0 +1,20 @@
+package clarusway.tests.Day_01;
+
+import org.testng.annotations.Test;
+
+public class C06_Description {
+
+
+    @Test(description = "Bu test sistemimizin login fonksiyonlarini kontrol eder")
+    public void loginTest(){
+
+    }
+
+    @Test(description = "Bu test sistemimizin Home Page fonksiyonlarini kontrol eder")
+    public void homePageTest(){
+
+    }
+
+
+
+}
