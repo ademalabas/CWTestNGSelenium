93df19c277e60b9ad3064e346f9ac2fce70e5549
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-02 21:51:20 +0300

    02.01.23

---
+++ src/test/java/clarusway/tests/Day_03/C05_DataProviderExample.java
@@ -0,0 +1,53 @@
+package clarusway.tests.Day_03;
+
+import clarusway.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.nio.file.WatchEvent;
+
+public class C05_DataProviderExample extends TestBase {
+
+/*
+Open the site: https://www.amazon.com/
+Do it with DataProvider for
+Java
+JavaScript
+Phyton
+ */
+
+    @Test(dataProvider = "keyData")
+    public void amazonSearchTest(String key){
+        driver.get("https://www.amazon.com/");
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+
+        // DataProvider ile saglanan string ifade arama kismina yazilir ve enter a basilir
+        searchBox.sendKeys(key + Keys.ENTER);
+
+        WebElement searchResult = driver.findElement(By.className("a-color-state"));
+
+        // Arama sonucunun aradigimiz kelimeyi icerip icermedigi kontrol edilir
+        Assert.assertTrue(searchResult.getText().contains(key));
+
+
+
+
+    }
+
+    @DataProvider
+    public Object[][] keyData(){
+
+        return new Object[][]{
+                {"java"},
+                {"javascript"},
+                {"python"}
+        };
+    }
+
+
+
+}
