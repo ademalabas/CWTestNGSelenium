4db1eb0f51e200f422a689928dc0e8314e6ac66f
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-02 21:14:20 +0300

    02.01.23

--- src/test/java/clarusway/tests/Day_03/C01_DataProvider.java
+++ src/test/java/clarusway/tests/Day_03/C01_DataProvider.java
@@ -7,12 +7,17 @@ public class C01_DataProvider {
 
     @Test(dataProvider = "dataProvider")
     public void test01(String name) {
+
+        //verileri dataProvider methodundan alarak calisacak olan test
+
+
         System.out.println(name);
 
 
     }
 @DataProvider
     public Object[][] dataProvider() {
+    // verileri teste saglayacak olan method
 
         Object[][] data = {
                 {"Adem"},
