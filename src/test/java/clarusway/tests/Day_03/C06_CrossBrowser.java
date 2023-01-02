4815d12b7134dd391e2372c791ee747463e7f0f7
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-02 22:21:32 +0300

    02.01.23

---
+++ src/test/java/clarusway/tests/Day_03/C06_CrossBrowser.java
@@ -0,0 +1,11 @@
+package clarusway.tests.Day_03;
+
+import clarusway.utilities.BaseCROSSBrowserTest;
+
+public class C06_CrossBrowser extends BaseCROSSBrowserTest {
+
+
+
+
+
+}
---
+++ src/test/java/clarusway/utilities/BaseCROSSBrowserTest.java
@@ -0,0 +1,54 @@
+package clarusway.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
+public class BaseCROSSBrowserTest {
+    protected WebDriver driver;
+    protected Actions actions;
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setUp(@Optional("chrome") String browser){
+
+        switch (browser){
+
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                driver = new SafariDriver();
+                break;
+
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        actions =new Actions(driver);
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+
+    }
+
+
+}
