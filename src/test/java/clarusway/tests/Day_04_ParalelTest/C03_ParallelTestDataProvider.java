7346083eb19e2c0c70abc7f6372c078612070017
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-03 21:45:15 +0300

    03.01.23

--- src/test/java/clarusway/tests/Day_04_ParalelTest/C03_ParallelTestDataProvider.java
+++ src/test/java/clarusway/tests/Day_04_ParalelTest/C03_ParallelTestDataProvider.java
@@ -1,8 +1,19 @@
 package clarusway.tests.Day_04_ParalelTest;
 
+import org.testng.annotations.Test;
+
 public class C03_ParallelTestDataProvider {
 
+@Test(dataProvider="getData")
+    public void test(String name, String surname){
 
+
+
+
+
+
+
+}
 
 
 
