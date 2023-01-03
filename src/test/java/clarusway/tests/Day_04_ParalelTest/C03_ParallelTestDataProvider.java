306198a2eb37ac28dc915125a78dfe24df1608c5
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-03 21:50:12 +0300

    03.01.23

--- src/test/java/clarusway/tests/Day_04_ParalelTest/C03_ParallelTestDataProvider.java
+++ src/test/java/clarusway/tests/Day_04_ParalelTest/C03_ParallelTestDataProvider.java
@@ -1,21 +1,25 @@
 package clarusway.tests.Day_04_ParalelTest;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class C03_ParallelTestDataProvider {
 
-@Test(dataProvider="getData")
-    public void test(String name, String surname){
+    @Test(dataProvider = "getData")
+    public void test(String name, String surname) {
+        System.out.println(name + " " + surname);
 
 
+    }
+
+    @DataProvider(parallel = true)
+    public Object[][] getData() {
+        return new Object[][]{
+                {"Adem", "Alabas"},
+                {"Handan", "Sarah"},
+                {"Nalan", "Sarah"}
 
 
-
-
-
-}
-
-
-
-
+        };
+    }
 }
